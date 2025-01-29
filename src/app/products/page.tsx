3afab0ccf98c9130c27ea3f066be1ea471548289
gabriel-mend <crowofcode@gmail.com>
2025-01-29 07:28:47 -0300

    feat: adding table component and create a product page with prisma

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,25 @@
-export default function ProductsPage() {
+import { db } from "@/lib/prisma";
+import { Button } from "../_components/ui/button";
+import { Plus } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumns } from "./_components/table-columns";
+
+export default async function ProductsPage() {
+  const products = await db.product.findMany()
   return (
-    <div>Produtos</div>
+    <div className="w-full space-y-8 p-8 bg-white mx-8 my-8 rounded-lg">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">Gestão de produtos</span>
+          <h2 className="text-2xl font-semibold">Produtos</h2>
+        </div>
+
+        <Button>
+          <Plus size={20} />
+          Novos produtos
+        </Button>
+      </div>
+      <DataTable columns={productTableColumns} data={products} />
+    </div>
   );
 }
