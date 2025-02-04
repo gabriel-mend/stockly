f81a226991c6c941b01830ee76d443bef1cb4f2e
gabriel-mend <crowofcode@gmail.com>
2025-02-03 22:48:28 -0300

    WIP: studying ISR components and Data caching

--- src/app/_data-access/product/get-products.ts
+++ src/app/_data-access/product/get-products.ts
@@ -2,6 +2,7 @@ import 'server-only'
 
 export async function getProducts() {
   const response = await fetch('http://localhost:3000/api/products', {
+    // se nao houver um no cache o codigo cai em request memorization
     method: 'GET',
     cache: 'no-cache'
   })
