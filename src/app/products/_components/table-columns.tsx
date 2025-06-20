081edac978c4dc6a8678f1ab29ac6c8ad97c11bb
gabriel-mend <crowofcode@gmail.com>
2025-06-20 10:06:27 -0300

    feat: adding a menu dropdown in table products

--- src/app/products/_components/table-columns.tsx
+++ src/app/products/_components/table-columns.tsx
@@ -1,17 +1,32 @@
-"use client"
+"use client";
 
-import { Badge } from "@/app/_components/ui/badge"
-import { Product } from "@prisma/client"
-import { ColumnDef } from "@tanstack/react-table"
-import { CircleIcon } from "lucide-react"
+import { Badge } from "@/app/_components/ui/badge";
+import { Button } from "@/app/_components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  CircleIcon,
+  ClipboardCopyIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+  TrashIcon,
+} from "lucide-react";
 
 const getStatusLabel = (status: string) => {
-  if (status === 'IN_STOCK') {
-    return 'Em estoque'
+  if (status === "IN_STOCK") {
+    return "Em estoque";
   }
-  return 'Fora de estoque'
-}
- 
+  return "Fora de estoque";
+};
+
 export const productTableColumns: ColumnDef<Product>[] = [
   {
     accessorKey: "name",
@@ -30,15 +45,56 @@ export const productTableColumns: ColumnDef<Product>[] = [
     header: "Status",
     cell: (row) => {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const product = row.row.original as any
-      const label = getStatusLabel(product?.status)
-      return <Badge variant={label === 'Em estoque' ? 'default' : 'outline'} className="gap-5">
-          <CircleIcon 
+      const product = row.row.original as any;
+      const label = getStatusLabel(product?.status);
+      return (
+        <Badge
+          variant={label === "Em estoque" ? "default" : "outline"}
+          className="gap-5"
+        >
+          <CircleIcon
             size={14}
-            className={`${label === 'Em estoque' ? 'fill-primary-foreground' : 'fill-destructive-foreground'}`}
+            className={`${label === "Em estoque" ? "fill-primary-foreground" : "fill-destructive-foreground"}`}
           />
           {label}
         </Badge>
-    }
+      );
+    },
   },
-]
+  {
+    header: "Ações",
+    accessorKey: "actions",
+    cell: (row) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const product = row.row.original as any;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Acoes</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="gap-1.5"
+              onClick={() => navigator.clipboard.writeText(product.id)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <EditIcon size={16} />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <TrashIcon size={16} />
+              Deletar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
